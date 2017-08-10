a53b32c8d899928ae571c1a8ede247e8be231082
Stephen Schools <stephen.schools>
2017-08-10 13:05:15 -0400

    number deck created

--- main.js
+++ main.js
@@ -12,5 +12,39 @@ function createDeck() {
     deck.push("K" + suites[j]);
     deck.push("A" + suites[j]);
   }
-  return deck;
+  return shuffleDeck(deck);
 }
+
+function shuffleDeck(deck) {
+  for (let i = deck.length - 1; i > 0; i--) {
+    let x = Math.floor(Math.random() * i);
+    let temp = deck[x];
+    deck[x] = deck[i];
+    deck[i] = temp;
+  }
+  return(deck);
+}
+
+function makeValueDeck(deck) {
+  let valueDeck = [];
+  for (let i = 0; i < deck.length; i++) {
+    let num = deck[i].slice(0,1);
+    if (num === "A") {
+      valueDeck[i] = 14;
+    } else if (num === "K") {
+      valueDeck[i] = 13;
+    } else if (num === "Q") {
+      valueDeck[i] = 12;
+    } else if (num === "J") {
+      valueDeck[i] = 11;
+    } else {
+      valueDeck[i] = parseInt(deck[i]);
+    }
+  }
+  return valueDeck;
+}
+
+let freshDeck = createDeck();
+let numDeck = makeValueDeck(freshDeck);
+console.log(freshDeck);
+console.log(numDeck);
