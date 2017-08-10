3f92509d128166c002c56fb1bc3529aa566f680b
Stephen Schools <stephen.schools>
2017-08-10 14:44:06 -0400

    game play working

--- main.js
+++ main.js
@@ -46,5 +46,63 @@ function makeValueDeck(deck) {
 
 let freshDeck = createDeck();
 let numDeck = makeValueDeck(freshDeck);
-console.log(freshDeck);
-console.log(numDeck);
+
+let compDeck = numDeck.slice(0,26);
+let playerDeck = numDeck.slice(26);
+console.log(compDeck.length);
+console.log(playerDeck.length);
+
+let warDeck = [];
+let warNum = 0;
+let numHands = 0;
+
+function war(p, c) {
+  console.log("war");
+  warNum += 1;
+  warDeck.push(p);
+  warDeck.push(c);
+  let numCards = 3;
+  if (playerDeck.length < 4) {
+    numCards = playerDeck.length - 1;
+  } else if (compDeck.length < 4) {
+    numCards = compDeck.length - 1;
+  }
+  for (let i = 0; i < numCards; i++) {
+    warDeck.push(playerDeck.pop());
+    warDeck.push(compDeck.pop());
+  }
+  playHand();
+}
+
+function playHand() {
+  numHands += 1;
+  let playerCard = playerDeck.pop();
+  let compCard = compDeck.pop();
+  if (playerCard > compCard) {
+    playerDeck.unshift(playerCard, compCard);
+    if (warDeck.length) {
+      for (let i = 0; i < warDeck.length; i++) {
+        playerDeck.unshift(warDeck[i]);
+      }
+      warDeck = [];
+    }
+  } else if (compCard > playerCard) {
+    compDeck.unshift(compCard, playerCard);
+    if (warDeck.length) {
+      for (let i = 0; i < warDeck.length; i++) {
+        playerDeck.unshift(warDeck[i]);
+      }
+      warDeck = [];
+    }
+  } else {
+    if (playerDeck.length && compDeck.length) {
+      war(playerCard, compCard);
+    }
+  }
+}
+
+while (playerDeck.length > 0 && compDeck.length > 0) {
+  playHand();
+  console.log(playerDeck.length + " " + compDeck.length);
+}
+console.log("Num Hands " + numHands + " NumWars " + warNum);
